72bd1437d8b12e42e059e9ac4ea11df970707808
Ali Sajid <alisajidcs@gmail.com>
2022-06-06 00:01:38 +0500

    add nav on home page

---
+++ modules/home/data.ts
@@ -0,0 +1,23 @@
+export const PATHS = {
+  shootingStar: 'shooting-star',
+  blog: 'blog',
+  three: 'three',
+}
+
+export const HOME_NAV = [
+  {
+    path: PATHS.shootingStar,
+    heading: 'Shooting Star',
+    subHeading: "Let's shoot a star!",
+  },
+  {
+    path: PATHS.blog,
+    heading: 'Blog',
+    subHeading: 'Create a post and share it with the world',
+  },
+  {
+    path: PATHS.three,
+    heading: 'Three.js Demo',
+    subHeading: 'See the wonders of three.js with your own eyes',
+  },
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
+import { HOME_NAV } from '../modules/home/data'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -18,11 +20,17 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
+          {HOME_NAV.map(({ path, heading, subHeading = '' }) => (
+            <a key={path} href={path} className={styles.card}>
+              <h2>{heading} &rarr;</h2>
+              <p>{subHeading}</p>
+            </a>
+          ))}
+
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -33,10 +41,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -46,9 +51,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
