28581f9214afc50bae3b73d971655dfb39ea74ec
Majid K <contactmakq@gmail.com>
2022-06-15 23:29:41 +0500

    added chakra/basic layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+import Nav from '../components/Navbar'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Nav />
+      <Component {...pageProps} />{' '}
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
